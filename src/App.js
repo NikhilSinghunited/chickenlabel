464644d6c75b4bcf9239a06f093a341bb4b60f94
nikhil singh nikhil singh <nikhilbhu32@gmail.com>
2023-11-15 11:56:51 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 // import Navbar from './components/pages/Navbar';
 import Card from './pages/Card';
 
-
 function App() {
   return (
-    <div className="App">
-      <Card name='nikhil' mobile='12323434345'  email='abc@mail.com' branch='BTech'/>
+    <div className='App'>
+      <Card
+        name='nikhil'
+        mobile='12323434345'
+        email='abc@mail.com'
+        branch='BTech'
+      />
     </div>
   );
 }
